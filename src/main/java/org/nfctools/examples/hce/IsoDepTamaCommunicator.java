e72646d82520f8d66b09d9f50b963d46dae53312
Adrian Stabiszewski <as@grundid.de>
2013-11-08 10:57:53 +0100

    clean up

--- src/main/java/org/nfctools/examples/hce/IsoDepTamaCommunicator.java
+++ src/main/java/org/nfctools/examples/hce/IsoDepTamaCommunicator.java
@@ -10,13 +10,27 @@ import org.nfctools.spi.tama.request.InListPassiveTargetReq;
 import org.nfctools.spi.tama.response.DataExchangeResp;
 import org.nfctools.spi.tama.response.InListPassiveTargetResp;
 import org.nfctools.utils.NfcUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class IsoDepTamaCommunicator extends AbstractTamaCommunicator {
 
+	private Logger log = LoggerFactory.getLogger(getClass());
 	private int messageCounter = 0;
+	private static final byte[] CLA_INS_P1_P2 = { 0x00, (byte)0xA4, 0x04, 0x00 };
+	private static final byte[] AID_ANDROID = { (byte)0xF0, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
 
 	public IsoDepTamaCommunicator(ByteArrayReader reader, ByteArrayWriter writer) {
 		super(reader, writer);
+	}
+
+	private byte[] createSelectAidApdu(byte[] aid) {
+		byte[] result = new byte[6 + aid.length];
+		System.arraycopy(CLA_INS_P1_P2, 0, result, 0, CLA_INS_P1_P2.length);
+		result[4] = (byte)aid.length;
+		System.arraycopy(aid, 0, result, 5, aid.length);
+		result[result.length - 1] = 0;
+		return result;
 	}
 
 	public void connectAsInitiator() throws IOException {
@@ -24,24 +38,20 @@ public class IsoDepTamaCommunicator extends AbstractTamaCommunicator {
 			InListPassiveTargetResp inListPassiveTargetResp = sendMessage(new InListPassiveTargetReq((byte)1, (byte)0,
 					new byte[0]));
 			if (inListPassiveTargetResp.getNumberOfTargets() > 0) {
-				System.out
-						.println("TargetData: " + NfcUtils.convertBinToASCII(inListPassiveTargetResp.getTargetData()));
+				log.info("TargetData: " + NfcUtils.convertBinToASCII(inListPassiveTargetResp.getTargetData()));
 				if (inListPassiveTargetResp.isIsoDepSupported()) {
-					System.out.println("IsoDep Supported");
-					byte[] dataOut = NfcUtils.convertASCIIToBin("00A4040007F001020304050600");
+					log.info("IsoDep Supported");
+					byte[] selectAidApdu = createSelectAidApdu(AID_ANDROID);
 					DataExchangeResp resp = sendMessage(new DataExchangeReq(inListPassiveTargetResp.getTargetId(),
-							false, dataOut, 0, dataOut.length));
-					String data = new String(resp.getDataOut());
-					System.out.println("Received: " + data);
-					if (data.startsWith("Hello")) {
-						while (true) {
-							dataOut = ("Message from desktop: " + messageCounter++).getBytes();
-							resp = sendMessage(new DataExchangeReq(inListPassiveTargetResp.getTargetId(), false,
-									dataOut, 0, dataOut.length));
-							data = new String(resp.getDataOut());
-							System.out.println("Received: " + data);
-						}
+							false, selectAidApdu, 0, selectAidApdu.length));
+					String dataIn = new String(resp.getDataOut());
+					log.info("Received: " + dataIn);
+					if (dataIn.startsWith("Hello")) {
+						exchangeData(inListPassiveTargetResp);
 					}
+				}
+				else {
+					log.info("IsoDep NOT Supported");
 				}
 				break;
 			}
@@ -53,6 +63,18 @@ public class IsoDepTamaCommunicator extends AbstractTamaCommunicator {
 					throw new RuntimeException(e);
 				}
 			}
+		}
+	}
+
+	private void exchangeData(InListPassiveTargetResp inListPassiveTargetResp) throws IOException {
+		DataExchangeResp resp;
+		String dataIn;
+		while (true) {
+			byte[] dataOut = ("Message from desktop: " + messageCounter++).getBytes();
+			resp = sendMessage(new DataExchangeReq(inListPassiveTargetResp.getTargetId(), false, dataOut, 0,
+					dataOut.length));
+			dataIn = new String(resp.getDataOut());
+			log.info("Received: " + dataIn);
 		}
 	}
 }
